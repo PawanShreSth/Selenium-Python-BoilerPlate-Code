6b9ca466a26f12b25534aa8e0e84933511820315
Pawan Shrestha <pawanshrestha@lftechnology.com>
2024-05-21 13:54:47 +0545

    Used env variable for URL

--- test/login2_test.py
+++ test/login2_test.py
@@ -6,12 +6,11 @@ load_environment_variables()
 
 def test_login(driver):
     # Example of accessing password from environment variable file
-    password = "hehehhe"
-    base_url = os.getenv("BASE_URL")
+    base_url = os.getenv("OTHER_URL")
     
     login = Login(driver)
 
-    login.visit_page("https://practice.expandtesting.com/login")
+    login.visit_page(base_url)
     login.enter_username_2()
     login.enter_password_2()
     login.click_submit_2()
