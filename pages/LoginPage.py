6c1c28c86fb0e4f75930080f46baa21afbcb72e9
Pawan Shrestha <pawanshrestha@lftechnology.com>
2024-06-23 13:40:31 +0545

    changed variable name

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -31,35 +31,35 @@ class Login:
     
     # Fo login_test.py
     def enter_username(self):
-        locator_strategy_value = (By.CSS_SELECTOR, self.email_field_locator_value)
+        locator_type_value = (By.CSS_SELECTOR, self.email_field_locator_value)
 
         # Passing use_wait and wait_time is optional
         # if use_wait is not passed by default it is False meaning no wait will be applied.
-        # self.element.get_element(locator_strategy_value, True, 5) Here the element is explicitly waitied and 5 second wait time is applied
+        # self.element.get_element(locator_type_value, True, 5) Here the element is explicitly waitied and 5 second wait time is applied
 
-        email_field = self.element.get_element(locator_strategy_value)
+        email_field = self.element.get_element(locator_type_value)
         email_field.send_keys("student")
 
 
     def enter_password(self, password: str):
-        locator_strategy_value = (By.CSS_SELECTOR, self.password_field_locator_value)
+        locator_type_value = (By.CSS_SELECTOR, self.password_field_locator_value)
 
-        password_field = self.element.get_element(locator_strategy_value)
+        password_field = self.element.get_element(locator_type_value)
         password_field.send_keys(password)
 
 
     def click_submit(self):
-        locator_strategy_value = (By.XPATH, self.button_locator_value)
+        locator_type_value = (By.XPATH, self.button_locator_value)
 
         # Using explicit wait
-        button = self.element.get_element(locator_strategy_value, use_wait=True, wait_time=5)
+        button = self.element.get_element(locator_type_value, use_wait=True, wait_time=5)
         button.click()
 
     
     def checkLoggedInMessage(self):
-        locator_strategy_value = (By.CSS_SELECTOR, self.logged_in_msg_locator_value)
+        locator_type_value = (By.CSS_SELECTOR, self.logged_in_msg_locator_value)
 
-        heading_element = self.element.get_element(locator_strategy_value, wait_time=True)
+        heading_element = self.element.get_element(locator_type_value, wait_time=True)
 
         is_visible = heading_element.is_displayed()
 
@@ -69,24 +69,24 @@ class Login:
     
     # Fo login2_test.py
     def enter_username_2(self):
-        locator_strategy_value = (By.CSS_SELECTOR, self.email_field_locator_value)
+        locator_type_value = (By.CSS_SELECTOR, self.email_field_locator_value)
 
-        email_field = self.element.get_element(locator_strategy_value, use_wait=True)
+        email_field = self.element.get_element(locator_type_value, use_wait=True)
         email_field.send_keys("practice")
 
 
     def enter_password_2(self):
-        locator_strategy_value = (By.CSS_SELECTOR, self.password_field_locator_value)
+        locator_type_value = (By.CSS_SELECTOR, self.password_field_locator_value)
 
 
-        password_field = self.element.get_element(locator_strategy_value)
+        password_field = self.element.get_element(locator_type_value)
         password_field.send_keys("SuperSecretPassword!")
 
     
     def click_submit_2(self):
-        locator_strategy_value = (By.CSS_SELECTOR, self.button_locator_value_2)
+        locator_type_value = (By.CSS_SELECTOR, self.button_locator_value_2)
 
-        button = self.element.get_element(locator_strategy_value)
+        button = self.element.get_element(locator_type_value)
         button.click()
 
 
